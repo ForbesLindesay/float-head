0712b88d35ee282c75d1019c8029d96d3a16ae78
Dee Lindesay <dee@lindesay.co.uk>
2015-08-24 13:26:33 +0100

    Change route and rebuild

--- build.js
+++ build.js
@@ -31,7 +31,7 @@ var postfix = '\n' + [
   '};'
 ].join('\n');
 
-request('https://cdnjs.cloudflare.com/ajax/libs/floatthead/' + process.argv[2] + '/jquery.floatThead.js',
+request('https://raw.githubusercontent.com/mkoryak/floatThead/v' + process.argv[2] + '/dist/jquery.floatThead.js',
   function (err, res) {
     if (err) throw err;
     if (res.statusCode !== 200) throw new Error('status code: ' + res.statusCode);
